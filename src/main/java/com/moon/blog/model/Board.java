ead965365dfd872b56651ef9d9a75d3d736c7d4d
voyager <cdspacenoob@gmail.com>
2022-11-04 20:14:05 +0900

    JPA

--- src/main/java/com/moon/blog/model/Board.java
+++ src/main/java/com/moon/blog/model/Board.java
@@ -1,5 +1,50 @@
 package com.moon.blog.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data							// getter setter
+@NoArgsConstructor		// 빈 생성자.
+@AllArgsConstructor		// 풀 생성자. 
+@Builder						// 선택적 생성자.
+@Entity
 public class Board {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)		// auto_increment
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob								// 매우 큰 대용량 데이터로 잡아줄 때 선언하는 어노테이션.
+	private String content;	// 라이브러리를 사용하면 예를 들어 에디터, <html> 태그가 섞이기 때문에 매우매우 길어질 수 있다.
+	
+	@ColumnDefault("0")		// int이기 때문에 "" 따옴표만 써준다. 
+	private int count;
+	
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private User user;			// Database에서는 OBJ를 저장할 수 없으므로 FK를 지정하여 잇는다. 하지만 Java는 OBJ를 저장할 수 있다.
+										// 따라서 OBJ에 대해 충돌이 일어날 수 있는데(Java는 OBJ를 던지고 DB는 OBJ를 안 받으려하고), ORM을 사용하여 오브젝트를 저장한다.
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 }
--- src/main/java/com/moon/blog/model/User.java
+++ src/main/java/com/moon/blog/model/User.java
@@ -2,24 +2,45 @@ package com.moon.blog.model;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.sql.ordering.antlr.GeneratedOrderByFragmentRendererTokenTypes;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data							// getter setter
+@NoArgsConstructor		// 빈 생성자.
+@AllArgsConstructor		// 풀 생성자. 
+@Builder						// 선택적 생성자.
 @Entity
 public class User {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	// 프로젝트에 연결된 DB 넘버링 전략을 따라간다.
-	private int id;						// Orace: sequence로 처리, MySQL: auto_increment로 처리.
+	@Id																					// PK로 설정.
+	@GeneratedValue(strategy = GenerationType.IDENTITY)		// 프로젝트에 연결된 DB 넘버링 전략을 따라간다.
+	private int id;													// Orace: sequence로 처리, MySQL: auto_increment로 처리.
 	
-	private String username;	// id.
+	@Column(nullable = false, length = 30)			// null이 될 수 없음을 의미, 최대 30글자를 넘을 수 없다. 
+	private String username;	
 	
+	@Column(nullable = false, length = 100)			// 왜 100이나 주는가? 이후에 Hash로 변환하여 암호화하기 위해서다. 따라서 넉넉하게 100 준다. 
 	private String password;
 	
+	@Column(nullable = false, length = 50)			
 	private String email;
 	
+	@ColumnDefault(" 'user' ")								// ColumnDefault를 사용할 때는 따옴표 사용에 주의한다. 
+	private String role;											// 정확하게 하자면 Enum을 써야 한다. 데이터의 도메인을 만들어 줄 수 있다. admin, user, manager 등...
+																		// String을 쓰면 오타가 날 수 있기 때문에, 가능하다면 Enum을 사용해준다. 
+	
+	@CreationTimestamp										// 이 값들이 insert 되는 시점의 시간이 자동으로 입력된다. (insert문에서 비워두면 된다)
 	private Timestamp createDate;
 }
