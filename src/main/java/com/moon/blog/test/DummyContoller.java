a79305c52787a12d8f4c686b36b01e7f814ddae6
voyager <cdspacenoob@gmail.com>
2022-11-10 23:33:39 +0900

    selectAll, paging

--- src/main/java/com/moon/blog/test/DummyContoller.java
+++ src/main/java/com/moon/blog/test/DummyContoller.java
@@ -1,9 +1,14 @@
 package com.moon.blog.test;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +24,22 @@ public class DummyContoller {
 	
 	@Autowired	// DummyController가 사용될 때, 아래의 객체도 함께 생성되어 사용된다. (의존성 주입)
 	private UserRepository userRepository;
+	
+	// http://localhost:8000/blog/dummy/user/
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한 페이지에 2개의 데이터 받아보기.
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
+//		Page<User> users = userRepository.findAll(pageable);
+//		List<User> users = userRepository.findAll(pageable).getContent();
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
 
 	// {id} 주소로 파라미터를 전달받을 수 있다.
 	// http://localhost:8000/blog/dummy/user/3
@@ -47,8 +68,13 @@ public class DummyContoller {
 		
 		// 람다로 표현할 수도 있다.
 //		User user = userRepository.findById(id).orElseThrow(()->{
-//			return new IllegalArgumentException("해당 유저 "+id+"는 존재하지 않습니다.");		// mesage를 담을 수 있다.
+//			return new IllegalArgumentException("해당 유저 "+id+"는 존재하지 않습니다.");		
 //		}); 
+		
+		// 요청은 웹 브라우저가 했는데 객체를 던져주면 이상해진다.
+		// 따라서 이 객체를 웹 브라우저가 이해할 수 있는 데이터로 변환하는데 이 형식이 Json임.
+		// SpringBoot에는 MessageConverter라는 애가 응답 시에 자동으로 작동한다.
+		// 만약에 Java Obj를 return하면 MessageConverter가 Jackson을 호출하여 자동으로 변환하여 던져준다. 
 		return user;
 	}
 	
@@ -62,8 +88,8 @@ public class DummyContoller {
 			System.out.println("role: " + user.getRole());
 			System.out.println("createDate: " + user.getCreateDate());
 			
-			user.setRole(RoleType.USER);
-			userRepository.save(user);		// 이렇게만 해주면 user 객체를 user 테이블에 알아서 매핑하고 저장해준다.(insert)
+			user.setRole(RoleType.USER);		// 오타 내면 안 되니까 정해진 값을 쓰도록 한다. 
+			userRepository.save(user);			// 이렇게만 해주면 user 객체를 user 테이블에 알아서 매핑하고 저장해준다.(insert)
 		return "회원가입이 완료되었습니다.";
 	}
 	
